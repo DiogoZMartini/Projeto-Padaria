293b33c9e8239cb9417c78ea5406c5d7a2173802
DiogoZMartini <diogo.81226211@gmail.com>
2023-09-16 18:34:15 -0300

    Criação dos atributos da Classe Produto

--- src/java/br/confeitariaCelestial/DTO/Produto.java
+++ src/java/br/confeitariaCelestial/DTO/Produto.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package br.confeitariaCelestial.DTO;
-
-/**
- *
- * @author diogo
- */
 public class Produto {
+    private int id;
+    private String nome;
+    private double preco;
+    private int quantidade;
     
 }
