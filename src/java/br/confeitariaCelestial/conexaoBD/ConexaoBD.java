5f376765e0445a1de12a9ed533e798f96b974ba2
DiogoZMartini <diogo.81226211@gmail.com>
2023-09-18 13:32:53 -0300

    Atualização na classe ConexaoBD, adição do metodo getConexao()

--- src/java/br/confeitariaCelestial/conexaoBD/ConexaoBD.java
+++ src/java/br/confeitariaCelestial/conexaoBD/ConexaoBD.java
@@ -1,7 +1,23 @@
 package br.confeitariaCelestial.conexaoBD;
 
 import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 public class ConexaoBD {
     private Connection conexao = null;
+
+//Fazer a Conexão do banco com o site
+    public Connection getConexao() throws ClassNotFoundException{
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/(Banco)?user=root&password=";
+            this.conexao = (Connection) DriverManager.getConnection(url);
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "Erro na classe ConexaoBD e no metodo getConexao()");
+        }
+        return this.conexao;
+    
+    }
 }
